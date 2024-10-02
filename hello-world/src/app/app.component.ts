397bd1f867b7cea47d60d1101a14b39109e8f614
Romain Cochonnat <romain.cochonnat@gmail.com>
2024-10-02 16:21:44 +0200

    premiers pas angular

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImagecomponentComponent } from './imagecomponent/imagecomponent.component';
 import { TextcomponentComponent } from './textcomponent/textcomponent.component'
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TextcomponentComponent],
+  imports: [RouterOutlet, TextcomponentComponent, ImagecomponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
